6b65bcef66f25d2648102dd0e2aebf4cc28d3ec8
JurreM-dev <269440156+JurreM-dev@users.noreply.github.com>
2026-05-12 22:07:09 +0200

    Create Monsterlist with common, uncommon, and rare monsters
    Added a list of monsters with various rarities and descriptions.

---
+++ V1/Monsterlist.js
@@ -0,0 +1,83 @@
+    let monsters = [
+      
+      //common
+   {name: "Earoleaf",
+    rarity: "common",
+    description: "A leaf like creature flying through the air.",
+    type: "nature"},
+    
+    {name: "Blazevolt",
+    rarity: "common",
+    description: "A flame energy attacking through the web",
+    type: "fire"},
+    
+   {name: "emberrock",
+    rarity: "common",
+    description: "A creature that looks like a normal rock but in reality is a dangerous fire creature",
+    type: "fire"},
+    
+   {name: "groundplant",
+    rarity: "common",
+    description: "A plant creature that digs tunnels to move really quickly",
+    type: "nature"},
+    
+    {name: "windbite",
+    rarity: "common",
+    description: "A dog like creature that runs on the wind and through the skies",
+    type: "air"},
+    
+    //uncommon
+    {name: "Seashade",
+    rarity: "uncommon",
+    description: "A creature of shadows living in the depths of the ocean.",
+    type: "water"},
+    
+    {name: "waveflow",
+    rarity: "uncommon",
+    description: "a quick creature that can move through waves at insane speeds",
+    type: "water"},
+    
+    {name: "twigkick", 
+    rarity: "uncommon",
+    description: "A living twig that likes to kick its surroundings and attack leaves",
+    type: "nature"},
+    
+        {name: "colorfire",
+    rarity: "uncommon",
+    description: "A beautiful rainbow colored flame creature running around through the fields",
+    type: "fire"},
+    
+    //epic
+   {name: "pyroskies", 
+    rarity: "epic",
+    description: "A flaming fire dragon living high up in the clouds",
+    type: "fire"},
+    
+    {name: "thundercrash",
+    rarity: "epic",
+    description: "An insanely fast cheetah creature running through the sky and crashing sometimes creating thunder strikes",
+    type: "lightning"},
+    
+    ];
+    
+    
+    
+    
+    
+    
+    
+    
+    //ultra rares
+    let ultrararemonster = [
+      {name: "creagalaxy",
+      rarity: "ultra-rare",
+      description: "A gigantic dragon moving through the galaxy as if it's his private domain. Has the power to create and destroy anything",
+      type: "galaxy"}
+      ];
+      
+      let godlymonster = [
+   {name: "Wrath of Zeus",
+    rarity: "godly",
+    description: "A thundering dragon creature, soaring through the skies and destroying everything in it's way.",
+    type: "thunder"},
+      ]
