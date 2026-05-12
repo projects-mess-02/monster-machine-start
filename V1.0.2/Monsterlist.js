c1322a69b2ff9cef862fa1cce8ef33bce86ec368
JurreM-dev <269440156+JurreM-dev@users.noreply.github.com>
2026-05-12 22:10:46 +0200

    Add monster list with various rarity types
    Added a comprehensive list of monsters with various rarities including common, uncommon, epic, ultra-rare, godly, and hellbound types.

---
+++ V1.0.2/Monsterlist.js
@@ -0,0 +1,118 @@
+    let monsters = [
+      
+      //common
+   {name: "Earoleaf",
+    rarity: "common",
+    description: "A leaf like creature flying through the air.",
+    type: "🌱"},
+    
+    {name: "Blazevolt",
+    rarity: "common",
+    description: "A flame energy attacking through the web",
+    type: "🔥"},
+    
+   {name: "emberrock",
+    rarity: "common",
+    description: "A creature that looks like a normal rock but in reality is a dangerous fire creature",
+    type: "🔥"},
+    
+   {name: "groundplant",
+    rarity: "common",
+    description: "A plant creature that digs tunnels to move really quickly",
+    type: "🌱"},
+    
+    {name: "windbite",
+    rarity: "common",
+    description: "A dog like creature that runs on the wind and through the skies",
+    type: "🌪"},
+    
+    {name: "flamesight",
+    rarity: "common",
+    description: "A creature with a bunch of eyes that creates fire just by looking",
+    type: "🔥"}, 
+    
+    {name: "thundersect",
+    rarity: "common",
+    description: "An insect creature that climbes into trees and summons lightning upon it's enemies to kill them",
+    type: "⚡️"},
+    //uncommon
+    {name: "Seashade",
+    rarity: "uncommon",
+    description: "A creature of shadows living in the depths of the ocean.",
+    type: "💧"},
+    
+    {name: "waveflow",
+    rarity: "uncommon",
+    description: "a quick creature that can move through waves at insane speeds",
+    type: "💧"},
+    
+    {name: "twigkick", 
+    rarity: "uncommon",
+    description: "A living twig that likes to kick its surroundings and attack leaves",
+    type: "🌱"},
+    
+        {name: "colorfire",
+    rarity: "uncommon",
+    description: "A beautiful rainbow colored flame creature running around through the fields",
+    type: "🔥"},
+    
+    {name: "surf-ant",
+    rarity: "uncommon",
+    description: "An ant that makes surfboards out of little leafs and uses them to surf out on sea",
+    type: "💧"},
+    //epic
+   {name: "pyroskies", 
+    rarity: "epic",
+    description: "A flaming fire dragon living high up in the clouds",
+    type: "🔥"},
+    
+    {name: "thundercrash",
+    rarity: "epic",
+    description: "An insanely fast cheetah creature running through the sky and crashing sometimes creating thunder strikes",
+    type: "⚡️"},
+    
+    {name: "quicksky", 
+    rarity: "epic",
+    description: "A little creature that uses the wind to run really fast and jump really high, it uses it's power to race insects",
+    type: "🌪"},
+    
+    ];
+    
+    
+    
+    
+    
+    
+    
+    
+    //ultra rares
+    let ultrararemonster = [
+      {name: "creagalaxy",
+      rarity: "ultra-rare",
+      description: "A gigantic dragon moving through the galaxy as if it's his private domain. Has the power to create and destroy anything",
+      type: "🌌"},
+      
+      {name: "Earthcrusher",
+      rarity: "ultra-rare",
+      description: "A creature living in the core of the planet that eats through the underground with such power it creates earthquakes",
+      type: "🪨"},
+      ];
+      
+      let godlymonster = [
+   {name: "Wrath of Zeus",
+    rarity: "godly",
+    description: "A thundering dragon creature, soaring through the skies and destroying everything in it's way.",
+    type: "⚡️"},
+    
+        {name: "Poseidon's ruthlessness",
+    rarity: "godly",
+    description: "A giant horse creature that summons tidal waves and storms. It's one of the most powerful beings in the entire sea.",
+    type: "💧"},
+      ]
+      
+  let hellboundmonster = [
+    {name: "luciblaze",
+    rarity: "hellbound",
+    description: "A royal of hell, ruling over the sinners and torturing them, has the power of all demons and gets fed by the pure fear of those who will suffer eternal damnation.",
+    type: "🔥"}
+    ]
